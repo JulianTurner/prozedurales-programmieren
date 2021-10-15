35ee633981d559ec3d3d4974feb6291a0609ce42
JulianTurner <tuerner.j@gmail.com>
2021-10-15 18:42:44 +0200

    scanf

---
+++ scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int zahl1;
+
+  printf("Bitte geben Sie eine Zahl ein ");
+  scanf("%d", &zahl1);
+  printf("Sie haben %d eingegeben\n", zahl1);
+  return 0; // Beende ohne Fehler
+}
