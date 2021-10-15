7c511ea0b6ffe53e11687bfbea0d1182d9a809ab
JulianTurner <tuerner.j@gmail.com>
2021-10-15 20:17:30 +0200

    letzte ziffer

---
+++ letzte-ziffer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int zahl;
+
+  printf("Dieses Programm gibt die letzte Zahl einer mehrstelligen Ganzzahl "
+         "zurück\n");
+  printf("Bitte die Zahl eingeben: ");
+  scanf("%d", &zahl);
+
+  if (zahl > 0) {
+    printf("%d", zahl % 10);
+  } else {
+    printf("Die Zahl ist 0");
+  }
+
+  return 0;
+}
