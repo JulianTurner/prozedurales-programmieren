2592b6def4829af1cff5903c904ffbbd0feb89bc
JulianTurner <tuerner.j@gmail.com>
2021-10-16 12:24:50 +0200

    Rechtschreibfehler

--- vasilis/scanf.c
+++ vasilis/scanf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-int main () {
-    int zahl1;
+int main() {
+  int zahl1;
 
-    printf("Bitte geben Sie eine Zahlen ein: ");
-    scanf ("%d" , &zahl1);
-    printf("Sie haben %d eingegeben\n", zahl1);
-    return 0; // Beende Ohne Fehler
+  printf("Bitte geben Sie eine Zahlen ein: ");
+  scanf("%d", &zahl1);
+  printf("Sie haben %d eingegeben\n", zahl1);
+  return 0; // Beende ohne Fehler
 }
