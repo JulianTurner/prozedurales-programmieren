2d9ffb25cfe84f1cb319ad0dbe0b318442cc34a9
vasilis <vatsegkan.zournatsidis@gmail.com>
2021-10-17 20:23:53 +0200

    apok gelöscht

--- vasilis/scanf.c
+++ vasilis/scanf.c
@@ -6,5 +6,5 @@ int main() {
   printf("Bitte geben Sie eine Zahlen ein: ");
   scanf("%d", &zahl1);
   printf("Sie haben %d eingegeben\n", zahl1);
-  return 0; // Beende ohne Fehler /// apok 
+  return 0; // Beende ohne Fehler 
 }
