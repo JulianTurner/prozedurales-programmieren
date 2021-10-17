638e762cdd55b9d2ebd1e30f45e37e101866cc08
Kririts <92580058+Kririts@users.noreply.github.com>
2021-10-17 17:10:11 +0200

    Update letzte-ziffer.c

--- kristina/letzte-ziffer.c
+++ kristina/letzte-ziffer.c
@@ -6,6 +6,7 @@ int main() {
   printf("Pls input any number: ");
   scanf("%d", &zahl1); // bitte lies es
   printf("The last digit of your number is %d\n", zahl1 % 10);
-  return 0; // Beende ohne Fehler}
+  return 0; // Beende ohne Fehler
+}
 // oder:
 // if(zahl > 0){printf("%d\n", zahl%10);} else {printf("Die Zahl ist 0");}
