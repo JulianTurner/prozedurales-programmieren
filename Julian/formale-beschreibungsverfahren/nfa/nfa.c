150251ce8f36cd59588f75ad89a1dc60b04a9b01
JulianTurner <tuerner.j@gmail.com>
2021-10-21 20:44:10 +0200

    intial nfa

---
+++ Julian/formale-beschreibungsverfahren/nfa/nfa.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int q0() {
+  // if a
+  // for each a go
+  // a
+  // a
+  // if b
+  // for each b go
+  // b
+  // b
+
+  return 0;
+}
+
+int q1() {
+  // if a
+  // for each a go
+  // a
+  // a
+  // if b
+  // for each b go
+  // b
+  // b
+  return 0;
+}
+
+// das input wort wäre a a b
+int main() {
+  // startzustände []
+  // endzustände []
+
+  //
+  return 0;
+}
