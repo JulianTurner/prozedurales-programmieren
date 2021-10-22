2ae08a7cc5423af865d80c87e911168024e74064
JulianTurner <tuerner.j@gmail.com>
2021-10-22 15:26:16 +0200

    added node struct for nfa

--- Julian/formale-beschreibungsverfahren/nfa/nfa.c
+++ Julian/formale-beschreibungsverfahren/nfa/nfa.c
@@ -1,4 +1,11 @@
+#include <stdbool.h>
 #include <stdio.h>
+
+struct Node {
+  char name[3];
+  bool isStart;
+  bool isFinish;
+};
 
 int q0() {
   // if a
@@ -31,5 +38,11 @@ int main() {
   // endzustände []
 
   //
+  struct Node nodes[2] = {{"q1", true, false}, {"q2", false, true}};
+
+  for (int i = 0; i <= 2; i++) {
+    printf("%s\n", nodes[i].name);
+  }
+
   return 0;
 }
