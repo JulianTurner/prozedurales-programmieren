393649cdbc4ad3cc5a5e3c47d440ba68a9fc32e2
JulianTurner <tuerner.j@gmail.com>
2021-10-22 21:01:30 +0200

    added new datatypes to node

--- Julian/formale-beschreibungsverfahren/nfa/nfa.c
+++ Julian/formale-beschreibungsverfahren/nfa/nfa.c
@@ -1,11 +1,21 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-struct Node {
+typedef struct Delta {
+  char input[5];
+  char destination[5];
+} Delta;
+
+typedef struct Node {
   char name[3];
   bool isStart;
   bool isFinish;
-};
+  Delta deltas[256];
+  // q2, a
+  // q2, b
+  // q1, a
+  // array delta[] = [{q2, a}, {q2,b}]
+} Node;
 
 int q0() {
   // if a
@@ -38,10 +48,20 @@ int main() {
   // endzustände []
 
   //
-  struct Node nodes[2] = {{"q1", true, false}, {"q2", false, true}};
+  struct Node nodes[2] = {
+      {"q1", true, false, "a", "Q2", "b", "Q3"},
+      {"q2", true, false, "c", "Q5", "e", "Q6"},
 
-  for (int i = 0; i <= 2; i++) {
-    printf("%s\n", nodes[i].name);
+  };
+
+  for (int i = 0; i < 2; i++) {
+    Node node = nodes[i];
+    printf("\n%s\n", node.name);
+
+    for (int j = 0; j <= 1; j++) {
+      printf("Input: %s\n", node.deltas[j].input);
+      printf("Destination: %s\n", node.deltas[j].destination);
+    }
   }
 
   return 0;
