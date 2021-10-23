9784676b664d617ad6509348ea12c9eed65eced1
JulianTurner <tuerner.j@gmail.com>
2021-10-23 16:20:48 +0200

    added minmax

---
+++ Julian/for-loops/minmax.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // ein Programm dass eine vom Benutzer festgelegte Anzahl von Zahlen einliest
+  // und anwschließend die größte und die kleinste der eingegeben Zahlen auf dem
+  // Bildschirm ausgibt.
+  int arraySize = 3;
+  int zahlen[arraySize];
+  printf("Bitte %d Zahlen eingeben", arraySize);
+
+  for (int i = 0; i >= arraySize; i++) {
+    printf("%d", arraySize);
+  }
+
+  return 0;
+}
