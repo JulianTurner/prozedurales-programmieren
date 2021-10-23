8128c0049563f006b453834afcb5e4ca1754e95f
Furkan <F.eker@gmx.net>
2021-10-23 15:54:26 +0200

    remove int dm

--- Furkan/eurdm.c
+++ Furkan/eurdm.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main() {
     int eur;
-    float dm;
     float Wechselkurs = 1.95583;
 
         printf("Geben Sie einen Euro Betrag ein, um in DM umzurechnen: ");
         scanf("%d", &eur); 
-        printf("Das sind %f DM\n",&dm, eur * Wechselkurs);
+        printf("Das sind %f DM\n", eur * Wechselkurs);
     }
