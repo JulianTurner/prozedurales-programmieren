3869f4b9eabf873984f6e89e1849ec4596a084a8
Julian Turner <tuerner.j@gmail.com>
2021-10-25 14:08:31 +0200

    finished minmax

--- Julian/Übungsaufgaben/minmax.c
+++ Julian/Übungsaufgaben/minmax.c
@@ -4,13 +4,30 @@ int main() {
   // ein Programm dass eine vom Benutzer festgelegte Anzahl von Zahlen einliest
   // und anwschließend die größte und die kleinste der eingegeben Zahlen auf dem
   // Bildschirm ausgibt.
-  int arraySize = 3;
-  int zahlen[arraySize];
-  printf("Bitte %d Zahlen eingeben", arraySize);
+  int arraySize = 5, zahlen[arraySize], zahlHigh, zahlLow;
+  printf(
+      "Dieses Programm zeigt die höchte und niedgrigste Zahl aus %d Zahlen\n",
+      arraySize);
 
-  for (int i = 0; i >= arraySize; i++) {
-    printf("%d", arraySize);
+  for (int i = 0; i < arraySize; i++) {
+    int input;
+    printf("\n%d von %d: ", 1 + i, arraySize);
+    scanf("%d", &input);
+
+    if (zahlLow > input) {
+      zahlLow = input;
+    }
+    if (zahlHigh < input) {
+      zahlHigh = input;
+    }
+  }
+  printf("\nDie höchste Zahl ist %d und die niedgriste Zahl ist %d.\n",
+         zahlHigh, zahlLow);
+
+  if (zahlHigh == zahlLow) {
+    printf("Sehr kreativ.\n");
   }
 
+  printf("Das Prgramm ist zu ende.\n");
   return 0;
 }
