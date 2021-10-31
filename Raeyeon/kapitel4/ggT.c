86f21f6f7996d0e013be9776bbe6dbc7e1cd88a6
salami2 <jiinKang2@gmail.com>
2021-10-31 08:46:02 +0100

    ggT falsch

--- Raeyeon/kapitel4/ggT.c
+++ Raeyeon/kapitel4/ggT.c
@@ -2,42 +2,48 @@
 
 int main(){
 
-    int a, b, c, i;
+    int a, b, i;
 
     printf("Bitte geben Sie die erste Zahl ein: ");
     scanf("%d", &a);
-
     printf("Bitte geben Sie die zweite Zahl ein: ");
     scanf("%d", &b);
 
-    for(i=1; b<a; i++) {
+    i = 1;
 
+    if(a>b){
+
+    while(a>b){
         a -= b;
+        i++;
 
-        for(i=1; b>a; i++) {
-
-          if (a<b){
-            b -= a;
-          if (b==a){
-            printf("ggT : %d", b);
-        } if (b==a==1){
-            printf("Die haben keinen ggT");
-        } 
-
+        while (a<b){
+        b -= a;
+        i++;
         }
+    } 
+    } else {
 
+    while (a<b){
+        b -= a;
+        i++;
 
+        while(a>b){
+        a -= b;
+        i++;
         }
+    }
 
     }
 
-        
-    
+    if(a==b!=1){
+        printf("ggT : %d",a);
+    }
+    if(a==b==1){
+        printf("kein ggT");
+    }
 
 
-
-
-    
 
     return 0;
 }
