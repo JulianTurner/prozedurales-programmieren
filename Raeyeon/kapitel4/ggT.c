69cc2573ff16d79dc90a8c4476172329523a37a7
salami2 <jiinKang2@gmail.com>
2021-10-31 03:41:13 +0100

    größter gemeinsamer Teiler.c

---
+++ Raeyeon/kapitel4/ggT.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+    int a, b, c, i;
+
+    printf("Bitte geben Sie die erste Zahl ein: ");
+    scanf("%d", &a);
+
+    printf("Bitte geben Sie die zweite Zahl ein: ");
+    scanf("%d", &b);
+
+    
+
+    
+
+    return 0;
+}
