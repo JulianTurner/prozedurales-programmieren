34789eaa9e763ca780bb1f80b20df025ba00c2c7
salami2 <jiinKang2@gmail.com>
2021-10-31 21:13:57 +0100

    ziffer, befoerderung versuch

--- Raeyeon/kapitel4/befoerderung.c
+++ Raeyeon/kapitel4/befoerderung.c
@@ -4,13 +4,23 @@ int main() {
 
     int sd=40, osr=160, sr=1000, i;
 
-    for(i=1; )
+    i=1;
 
+    while(sr>0){
 
+        sd -= sd*20/100;   //ruhestand
+        osr -= osr*20/100;
+        sr -= sr*20/100;
 
+        sd += osr*10/100;   //beförderung
+        osr -= osr*10/100 - sr*20/100;
+        sr -= sr*20/100;
 
+        i++;
 
-    
+    }
+
+    printf("%d Jahre", 3*i);
 
     return 0;
 }
