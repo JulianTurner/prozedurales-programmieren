a38ae0229a8e6c89374dde11da91c28437742e88
salami2 <jiinKang2@gmail.com>
2021-11-06 06:04:11 +0100

    tautologie müde

--- Raeyeon/kapitel5/tautologien.c
+++ Raeyeon/kapitel5/tautologien.c
@@ -1,19 +1,29 @@
 #include<stdio.h>
 int main(){
-    int A, B, C, D;
+    int A, B, C, D, ok;
 
     printf("A B C D \n");
+
+    int links = (A&&(B&&C)==(A&&B)&&C);
+    int rechts = (A||(B||C)==(A||B)||C);
 
     for(A=0; A<=1; A++){
         for(B=0; B<=1; B++){
             for(C=0; C<=1; C++){
                 for(D=0; D<=1; D++){
                     printf("%d %d %d %d   ",A, B, C, D);
-                    printf("Assoziativgesetz : %d\n", (A&&(B&&C)==(A&&B)&&C)==(A||(B||C)==(A||B)||C));
-                    printf("          Kommutativgesetz : %d\n", (A&&B==B&&A)==(A||B==B||A));
+                    printf("Assoziativgesetz : %d\n", links==rechts);
+                    if(links!=rechts){
+                        ok = 0;}
                 }
             }
         }
+    }
+
+    if(ok == 0){
+        printf("keine Tautologie");
+    }else{
+        printf("Tautologie");
     }
 
     return 0;
