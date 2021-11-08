1860f346abfedee01a791ff1030e8a41020285b3
salami2 <jiinKang2@gmail.com>
2021-11-08 16:59:24 +0100

    Aufgabe 1 Kapitel 6

--- Raeyeon/kapitel6/buchzaehlen.c
+++ Raeyeon/kapitel6/buchzaehlen.c
@@ -1,1 +1,25 @@
 #include<stdio.h>
+
+int main(){
+
+    char string[100];
+    char buchstabe;
+    int i;
+    int j=0;
+
+    printf("Buchstabe : ");
+    scanf("%c", &buchstabe);
+
+    printf("Text : ");
+    scanf("%s", string);
+
+
+    for(i=0; string[i] !=0 ; i++){
+        if(string[i]==buchstabe){
+            j++;
+        }
+    }
+    printf("Buchstabe %c ist %d mal verwendet geworden.", buchstabe, j);
+    
+    return 0;
+}
