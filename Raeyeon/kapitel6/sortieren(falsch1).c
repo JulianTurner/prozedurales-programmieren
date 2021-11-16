1acc5d2997894c0148276516680132eaf337bcd5
salami2 <jiinKang2@gmail.com>
2021-11-16 21:57:15 +0100

    sortieren versuch1

---
+++ Raeyeon/kapitel6/sortieren(falsch1).c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main(){
+
+    int zahlen[10];
+    int x;
+
+    for(int i=0; i<10; i++){
+        printf("Gib die %d. Zahl ein : ", i+1);
+        scanf("%d", &zahlen[i]);
+    }
+
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            if(j<i)
+                if(zahlen[i]>zahlen[j])
+                    x = zahlen[i];
+                    zahlen[j]=zahlen[i];
+                    zahlen[i]=x;
+            if(j>i)
+                if(zahlen[i]<zahlen[j])
+                    x = zahlen[i];
+                    zahlen[j]=zahlen[i];
+                    zahlen[i]=x;
+        }
+    }
+
+    for(int i=0; i<10; i++){
+        printf("%d ", zahlen[i]);
+    }
+
+    return 0;
+}
